256c8ec444518c08ba9b0deefe526c2520983ea2
Louis Godfrey <lginn26@gmail.com>
2026-03-23 15:32:14 -0400

    More dialog menu testing

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 
 int main() {
     int status;
+    char* options[] = {"op1", "op2", "op3", "op4", "op5"};
     init_dialog(stdin, stdout);
-    status = dialog_fselect(
+    status = dialog_menu(
         "fpMod",
-        "",
-        0, 0
+        "choose a file/directory",
+        0, 0, 25,
+        5,
+        options
     );
     
     end_dialog();
+
     return status;
 }
